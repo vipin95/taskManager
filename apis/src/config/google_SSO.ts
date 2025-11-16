ea56a99320d5c9f38146751ed970fcd3f3ea3a85
Vipin mavi <vipinmavi@vinay-MacBook-Pro.local>
2025-11-16 07:56:42 +0530

    redirect URL changed

--- apis/src/config/google_SSO.ts
+++ apis/src/config/google_SSO.ts
@@ -5,8 +5,8 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "http://localhost:4000/auth/google/g-login",
+      clientSecret: process.env.SERVER_URL!,
+      callbackURL: process.env.SERVER_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
